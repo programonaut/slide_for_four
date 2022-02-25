3d473e575dff764eb374262b06437e5c39e15138
Maximilian Kürschner <dev@maximilian-kuerschner.de>
2022-02-25 20:49:35 +0100

    add: loading animation

--- frontend/lib/pages/wait.dart
+++ frontend/lib/pages/wait.dart
@@ -1,17 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_puzzle_hack/widgets/menu_button.dart';
 import 'package:provider/provider.dart';
 
 import '../helper/ws.dart';
 
-class Wait extends StatelessWidget {
+class Wait extends StatefulWidget {
   static const path = "wait";
   const Wait({Key? key}) : super(key: key);
 
   @override
+  State<Wait> createState() => _WaitState();
+}
+
+class _WaitState extends State<Wait> {
+
+  var waitText = "Wait";
+  var loadState = "";
+  var curr;
+
+  var startedText = "Both players connected!";
+
+  @override
+  void initState() {
+    super.initState();
+    wait();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    var ws = context.read<WS>();
-    return Container(
-      child: Text(ws.started ? "started" : "wait"),
+    return Scaffold(
+      body: Consumer<WS>(
+        builder: ((context, value, child) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(value.started ? startedText : curr, style: TextStyle(fontSize: 32),),
+                if (value.started) ...[
+                  MenuButton(text: "Begin!", onPressed: () => print("Connect"), fontSize: 36,),
+                ]
+              ],
+            ),
+          );
+        }),
+      ),
     );
+  }
+
+  void wait() async {
+    while (true) {
+      if (loadState.length == 3) {
+        loadState = "";
+      }
+      loadState += ".";
+      setState(() {
+        curr = waitText + loadState;
+      });
+      await Future.delayed(const Duration(seconds: 1), () {});
+    }
   }
 }
